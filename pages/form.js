8a611bd66a9acf3c01dbadd93d7ce93122add0c6
Manuel Digregorio <digregorio.mr@gmail.com>
2023-04-17 06:47:43 -0300

    add new product using formik and mui

--- pages/form.js
+++ pages/form.js
@@ -1,52 +1,96 @@
 import React from "react";
-import { useFormik } from "formik";
+import ReactDOM from "react-dom";
+import { Formik, Form, useField } from "formik";
+import * as Yup from "yup";
 
-const AddProduct = () => {
-  // Pass the useFormik() hook initial form values and a submit function that will
-  // be called when the form is submitted
-
-  const formik = useFormik({
-    initialValues: {
-      title: "",
-      img: "",
-      subTitle: "",
-      price: "",
-    },
-  });
+const MyTextInput = ({ label, ...props }) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input>. We can use field meta to show an error
+  // message if the field is invalid and it has been touched (i.e. visited)
+  const [field, meta] = useField(props);
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="title">Email Address</label>
-      <input
-        id="title"
-        name="title"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.title}
-      />
-      <input
-        id="img"
-        name="img"
-        type="url"
-        onChange={formik.handleChange}
-        value={formik.values.img}
-      />
-      <input
-        id="subTitle"
-        name="subTitle"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.subTitle}
-      />
-      <input
-        id="price"
-        name="price"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.price}
-      />
-
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
   );
 };
-export default AddProduct;
+
+// And now we can use these
+const SignupForm = () => {
+  return (
+    <>
+      <h1>Subscribe!</h1>
+      <Formik
+        initialValues={{
+          title: "",
+          img: "",
+          subTitle: "",
+          price: "",
+        }}
+        validationSchema={Yup.object({
+          title: Yup.string()
+            .max(15, "Must be 15 characters or less")
+            .required("Required"),
+          img: Yup.string()
+            .url("Enter valid URL")
+            .required("URL is required")
+            .transform((value, originalValue) => {
+              const url = new URL(originalValue);
+              return url.href;
+            }),
+          subTitle: Yup.string()
+            .max(15, "Must be 15 characters or less")
+            .required("Required"),
+          price: Yup.number()
+            .typeError("Price must be a number")
+            .positive("Price must be greater than zero")
+            .required("Price is required")
+            .transform((value, originalValue) => {
+              const numberValue = Number(originalValue);
+              if (isNaN(numberValue)) {
+                return value;
+              }
+              return new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(numberValue);
+            }),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form>
+          <MyTextInput label="Title" name="title" type="text" placeholder="" />
+
+          <MyTextInput
+            label="Image URL"
+            name="img"
+            type="text"
+            placeholder=""
+          />
+
+          <MyTextInput
+            label="Product Description"
+            name="subTitle"
+            type="text"
+            placeholder=""
+          />
+
+          <MyTextInput label="Price" name="price" type="text" placeholder="" />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </>
+  );
+};
+
+export default SignupForm;
--- pages/products/add-new-product.js
+++ pages/products/add-new-product.js
@@ -6,39 +6,61 @@ import Button from "@mui/material/Button";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
+import { ReactDOM } from "react-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+import { useFormik } from "formik";
 
-export default function AddNewProduct() {
-  const [title, setTitle] = useState("");
-  const [img, setImg] = useState("");
-  const [subTitle, setSubTitle] = useState("");
-  const [price, setPrice] = useState("");
+const validationSchema = yup.object({
+  title: yup
+    .string()
+    .max(15, "Must be 15 characters or less")
+    .required("Title is required"),
+  img: yup.string().url("Enter valid URL").required("URL is required"),
+  subTitle: yup
+    .string()
+    .max(15, "Must be 15 characters or less")
+    .required("Description required"),
+  price: yup
+    .string()
+    .max(15, "Must be 15 characters or less")
+    .required("Description required"),
+});
 
+const AddNewProduct = () => {
   const router = useRouter();
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    axios
-      .post("http://localhost:8000/products/", {
-        title,
-        img,
-        subTitle,
-        price,
-      })
-      .then((response) => {
-        console.log(response.data);
-        toast.success("Product added successfully", {
-          onClose: () => router.push("/products"),
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      img: "",
+      subTitle: "",
+      price: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      axios
+        .post("http://localhost:8000/products/", {
+          title: values.title,
+          img: values.img,
+          subTitle: values.subTitle,
+          price: values.price,
+        })
+        .then((response) => {
+          console.log(response.data);
+          toast.success("Product added successfully", {
+            onClose: () => router.push("/products"),
+          });
+        })
+        .catch((error) => {
+          console.error(error);
         });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+    },
+  });
 
   return (
     <Box
       component="form"
-      onSubmit={handleSubmit}
+      onSubmit={formik.handleSubmit}
       sx={{
         "& > :not(style)": { m: 1, width: "25ch" },
       }}
@@ -46,28 +68,36 @@ export default function AddNewProduct() {
       autoComplete="off"
     >
       <TextField
-        value={title}
+        id="title"
+        name="title"
+        value={formik.values.title}
         label="Title"
         variant="outlined"
-        onChange={(event) => setTitle(event.target.value)}
+        onChange={formik.handleChange}
       />
       <TextField
-        value={img}
+        id="img"
+        name="img"
+        value={formik.values.img}
         label="Image URL"
         variant="outlined"
-        onChange={(event) => setImg(event.target.value)}
+        onChange={formik.handleChange}
       />
       <TextField
-        value={subTitle}
+        id="subTitle"
+        name="subTitle"
+        value={formik.values.subTitle}
         label="Product Description"
         variant="outlined"
-        onChange={(event) => setSubTitle(event.target.value)}
+        onChange={formik.handleChange}
       />
       <TextField
-        value={price}
+        id="price"
+        name="price"
+        value={formik.values.price}
         label="Price"
         variant="outlined"
-        onChange={(event) => setPrice(event.target.value)}
+        onChange={formik.handleChange}
       />
 
       <Button type="submit" variant="contained" color="primary">
@@ -75,5 +105,54 @@ export default function AddNewProduct() {
       </Button>
     </Box>
   );
-}
+};
+
+export default AddNewProduct;
+
+// const [title, setTitle] = useState("");
+// const [img, setImg] = useState("");
+// const [subTitle, setSubTitle] = useState("");
+// const [price, setPrice] = useState("");
+
+//   return (
+//     <Box
+//       component="form"
+//       onSubmit={handleSubmit}
+//       sx={{
+//         "& > :not(style)": { m: 1, width: "25ch" },
+//       }}
+//       noValidate
+//       autoComplete="off"
+//     >
+//       <TextField
+//         value={title}
+//         label="Title"
+//         variant="outlined"
+//         onChange={(event) => setTitle(event.target.value)}
+//       />
+//       <TextField
+//         value={img}
+//         label="Image URL"
+//         variant="outlined"
+//         onChange={(event) => setImg(event.target.value)}
+//       />
+//       <TextField
+//         value={subTitle}
+//         label="Product Description"
+//         variant="outlined"
+//         onChange={(event) => setSubTitle(event.target.value)}
+//       />
+//       <TextField
+//         value={price}
+//         label="Price"
+//         variant="outlined"
+//         onChange={(event) => setPrice(event.target.value)}
+//       />
+
+//       <Button type="submit" variant="contained" color="primary">
+//         add new product
+//       </Button>
+//     </Box>
+//   );
+//
 // por ser type submit directamente trigerea el submit del form. esto es por propiedades de html. Esto es porque el BOX es "form"
